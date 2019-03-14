26c208553e3ace763fcd6aa7ab45eb123e439050
Jean Perrault <perrault.jean66gmail.com>
2019-03-14 12:48:45 +0100

    -

--- 03-constructeur/Car.php
+++ 03-constructeur/Car.php
@@ -5,24 +5,56 @@ class Car{
     private $color;
     private $brand;
     private $model;
-
+    private $roue;
 
     
-        public function __construct($brand, $model) 
+        public function __construct($brand, $model, $roue) 
         {
             $this->brand = $brand;
             $this->model = $model;
+            $this->roue = $roue;
         }
         public function setColor($color) 
         {
             $this->color = $color;
+            return $this;
         }
         function drive()
         {
-            return 'La '.$this->brand .' '.$this->model. ' roule sur ses 4 roues. ';
+            return 'La '.$this->brand .' '.$this->model. ' roule sur ses '.$this->roue. ' roues. ';
         }
         function klaxon()
         {
             return 'La voiture '.$this->color .' klaxonne.';
+        }
+}
+
+class Moto{
+
+    private $color;
+    private $brand;
+    private $model;
+    private $roue;
+
+    
+        public function __construct($brand, $model, $roue) 
+        {
+            $this->brand = $brand;
+            $this->model = $model;
+            $this->roue = $roue;
+        }
+    
+        public function setColor($color) 
+        {
+            $this->color = $color;
+            return $this;
+        }
+        function drive()
+        {
+            return 'La '.$this->brand .' '.$this->model. ' roule sur ses '.$this->roue. ' roues. ';
+        }
+        function klaxon()
+        {
+            return 'La moto '.$this->color .' fait du bruit.';
         }
 }
--- 03-constructeur/index.php
+++ 03-constructeur/index.php
@@ -1,13 +1,18 @@
 <?php
 include_once "Car.php";
 /**
- * 
  * une voiture possède 4 roues, une couleur, une marque et un modèle (4 attributs)
  * une voiture peut rouler, klaxonner (2 methodes)
  */
 
- $car = new Car('Citroen', 'DS3');
+ $car = new Car('Citroen', 'DS3',4);
  $car->setColor('rouge');
 
  echo $car->drive();?><br><?php //affiche "la Citroen DS3 roule sur ses 4 roues."
- echo $car->klaxon(); // affiche "la voiture rouge klaxonne."
+ echo $car->klaxon();?><br><?php // affiche "la voiture rouge klaxonne."
+
+ $moto = new Moto('Kawasaki', 'Stinger',2);
+ $moto->setColor('verte');
+
+ echo $moto->drive();?><br><?php //affiche "la Citroen DS3 roule sur ses 4 roues."
+ echo $moto->klaxon(); // affiche "la voiture rouge klaxonne."
