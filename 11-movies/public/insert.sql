0007de22d7e6c0ed4963fc3865f5810cf124a2db
Jean Perrault <perrault.jean66gmail.com>
2019-03-18 17:26:25 +0100

    -

---
+++ 11-movies/public/insert.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE `movie`;
+
+INSERT INTO `movie` (`title`, `synopsis`, `released_at`, `category`) VALUES
+('Le Parrain', 'Lorem ipsum', '1972-01-01', 'Film de gangsters'),
+('Orange Mécanique', 'Lorem ipsum', '1981-01-01', 'Drame'),
+('Heat', 'Lorem ipsum', '1995-01-01', 'Film de gangsters');
